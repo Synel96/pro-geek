62b46cbd4034c96021425e637a2f3f545149b462
Szilveszter Nemeth <nmszilveszter@gmail.com>
2025-10-10 14:54:16 +0200

    Delete backend directory

--- backend/progeek/blogapp/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import BlogPostListView, BlogPostDetailView
-
-urlpatterns = [
-    path('posts/', BlogPostListView.as_view(), name='blogpost-list'),
-    path('posts/<int:pk>/', BlogPostDetailView.as_view(), name='blogpost-detail'),
-]
